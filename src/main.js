7e91ab00f1575a4d2dfb2dcce503f0ac5c1c1948
meishadevs <meishadevs@gmail.com>
2022-10-31 16:37:59 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/'
+import i18n from './locales'
+import { VueAxios } from './utils/request'
+import ProLayout, { PageHeaderWrapper } from '@ant-design-vue/pro-layout'
+import themePluginConfig from '../config/themePluginConfig'
+
+// mock
+// WARNING: `mockjs` NOT SUPPORT `IE` PLEASE DO NOT USE IN `production` ENV.
+import './mock'
+
+import bootstrap from './core/bootstrap'
+
+// 使用懒加载组件
+import './core/lazy_use'
+
+// 权限控制
+import './permission'
+
+// 全局过滤器
+import './utils/filter'
+
+// 全局样式
+import './global.less'
+
+Vue.config.productionTip = false
+
+// mount axios to `Vue.$http` and `this.$http`
+Vue.use(VueAxios)
+// use pro-layout components
+Vue.component('pro-layout', ProLayout)
+Vue.component('page-container', PageHeaderWrapper)
+Vue.component('page-header-wrapper', PageHeaderWrapper)
+
+window.umi_plugin_ant_themeVar = themePluginConfig.theme
+
+new Vue({
+  router,
+  store,
+  i18n,
+  created: bootstrap,
+  render: h => h(App)
+}).$mount('#app')
