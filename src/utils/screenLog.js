025095b9333d7ea555126939f592a5a3752da760
meishadevs <meishadevs@gmail.com>
2022-11-08 15:37:02 +0800

    默认使用中文语言包

--- src/utils/screenLog.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export const printANSI = () => {
-  console.log('[store admin] created()')
-  console.log('%c感谢使用 store admin !', 'color: #000; font-size: 14px;    font-family: Hiragino Sans GB,Microsoft YaHei,\\\\5FAE\\8F6F\\96C5\\9ED1,Droid Sans Fallback,Source Sans,Wenquanyi Micro Hei,WenQuanYi Micro Hei Mono,WenQuanYi Zen Hei,Apple LiGothic Medium,SimHei,ST Heiti,WenQuanYi Zen Hei Sharp,sans-serif;')
-  console.log('%cThanks for using store admin !', 'color: #fff; font-size: 14px; font-weight: 300; text-shadow:#000 1px 0 0,#000 0 1px 0,#000 -1px 0 0,#000 0 -1px 0;')
-  console.log('')
-  console.log('')
-}
