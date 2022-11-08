a1ab1e898af69cd66a259534ae4c7b986187fd07
meishadevs <meishadevs@gmail.com>
2022-11-08 15:49:14 +0800

    修改 404 页面文字

--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -1,8 +1,8 @@
 <template>
-  <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+  <a-result status="404" title="404" sub-title="对不起，您访问的页面不存在。">
     <template #extra>
       <a-button type="primary" @click="toHome">
-        Back Home
+        回到首页
       </a-button>
     </template>
   </a-result>
@@ -11,6 +11,7 @@
 <script>
 export default {
   name: 'Exception404',
+
   methods: {
     toHome () {
       this.$router.push({ path: '/' })
