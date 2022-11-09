400accf04d44f33ffcbd9c96e75c632062b44026
meishadevs <meishadevs@gmail.com>
2022-11-09 09:34:05 +0800

    规范store下模块文件命名

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import app from './modules/app'
 import user from './modules/user'
 
 // 默认路由模式为静态路由
-import permission from './modules/static-router'
+import permission from './modules/permission'
 
 import getters from './getters'
 
